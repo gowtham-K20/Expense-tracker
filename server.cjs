72f7bea4c399ae5254a25e84f673272b227d84d5
gowtham-K20 <160317763+gowtham-K20@users.noreply.github.com>
2024-02-17 15:35:55 +0530

    Add files via upload

---
+++ server.cjs
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+// importing required functions
+const {connectToDb, getDb} = require('./db.cjs')
+
+const app = express()
+app.use(bodyParser.json())
+
+let db
+// Connecting to the DB
+connectToDb(function(error) {
+    if(!error) {
+        // Starting the server
+        app.listen(8000)
+        console.log('Listening on port 8000...')
+        db = getDb()
+    } else {
+        // Server would not start
+        console.log(error)
+    }
+})
+
+app.post('/add-entry', function(request, response) {
+    db.collection('expensedata').insertOne(request.body)
+    console.log(request.body)
+
+
+})
+
+
+app.use(express.static(__dirname))
